986cf04c0dbb70124f4adc2d8dcc35488f61a8fe
Ahmed <Ahmed.aldhahi.a@gmail.com>
2026-05-13 22:40:29 +0300

    final

--- prisma.config.ts
+++ prisma.config.ts
@@ -11,4 +11,7 @@ export default defineConfig({
   datasource: {
     url: process.env["DATABASE_URL"],
   },
+  migrate: {
+    directUrl: process.env["DIRECT_URL"],
+  },
 });
--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,15 +1,8 @@
-import 'dotenv/config';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    const adapter = new PrismaMariaDb(process.env.DATABASE_URL as string);
-    super({ adapter });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
